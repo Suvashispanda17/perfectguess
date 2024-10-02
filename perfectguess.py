30100e65ea71811b73962c33b76927dd75ebd400
Suvashispanda17 <pandasuvashis17@gmail.com>
2024-10-02 21:07:02 +0530

    perfectguess.py

--- perfectguess.py
+++ perfectguess.py
@@ -13,18 +13,25 @@ print('''
 
 ''')
 X= rn.randint(1,100)
-print(X)
+
 def PERFECT():
+    guess=1
     while True:
         GUESS = int(input("CAN YOU GUESS THE NUMBER?    NOOO YOU DON'T :"))
         if GUESS==X:
             cs.daemon(f"OHHH SHITT YOU DID IT ......THAT'S THE ACTUAL NUMBER  {X}")
+            print(f"NUMBER OF ATTEMPTS {guess} ")
             break
 
         elif(X<GUESS):
             cs.dragon("THE NUMBER IS TOO HIGHHH!")
+            guess +=1
         else:
             cs.cow("THE NUMBER IS TOO LOOOOO!")
+            guess +=1
             
 
+
 PERFECT()
+
+
